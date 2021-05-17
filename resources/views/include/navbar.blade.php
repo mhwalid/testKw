df4996fb3c37774265b8e32089a3f651dfc8f667
Walid TIGHADOUINI <walid-t@RAIC.local>
2021-05-17 11:18:20 +0200

    Footer CSS

--- resources/views/include/navbar.blade.php
+++ resources/views/include/navbar.blade.php
@@ -1,28 +1,26 @@
-<nav id="Nav" class="navbar navbar-expand-lg navbar-light indigo mb-4" style="background-color: #d8b908;" >
-    <a class="navbar-brand" href="{{ route('product.index') }}"><img style="width: 80px; heigth:80px" src="{{asset('asset/img/kw-distribution.jpg')}}" alt=""></a>
+<nav id="Nav" class="navbar navbar-expand-lg navbar-light indigo mb-4" style="background-color: white;" >
+    <a class="navbar-brand" href="{{ route('product.index') }}"><img style="width: 80px; heigth:80px" src="{{asset('asset/img/kw.png')}}" alt=""></a>
     <div class="col-4pt-1">
-        <a href="{{ route('cart.index') }}"> Panier <span
-                class="badge badge-light badge-pill">{{ Cart::content()->count() }} </span></a>
+        <a href="{{ route('cart.index') }}"> <i class="fas fa-shopping-cart"></i> <span
+                class="badge badge-pill badge-warning">{{ Cart::content()->count() }} </span></a>
     </div>
     <!-- Collapsible content -->
 
+
+    <div class="md-form my-0">
+        <input class="form-control" type="text" placeholder="Recherche" id="search" name="q"
+            value="{{ request()->q ?? '' }}">
+    </div>
 
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
 
         <form action="{{ route('search') }}" method="POST" class="form-inline ml-auto" onsubmit="traitForm(a)"
             id="SearchFrom">
             @csrf
-            <div class="md-form my-0">
-                <input class="form-control" type="text" placeholder="Recherche" id="search" name="q"
-                    value="{{ request()->q ?? '' }}">
-            </div>
+
             <button class="btn btn-outline-white btn-md my-0 ml-sm-2" onclick='showOld(event)'
                 type="text"></button>
         </form>
     </div>
     @include('include.login')
 </nav>
-<<<<<<< HEAD
-
-=======
->>>>>>> 814c9aa49d37dd5681d76ff0f6dddc5555a98d47
