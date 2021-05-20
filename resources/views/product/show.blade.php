c6ed26c454b4895b922eefab1bdf4ce609095764
Mrassi DHIA <mrassi@RAIC.local>
2021-05-20 16:13:44 +0200

    moinsinfo

--- resources/views/product/show.blade.php
+++ resources/views/product/show.blade.php
@@ -1,5 +1,31 @@
 @extends('layouts.app')
 @section('content')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     <div class="container  align-items-center mt-4">
         <div class="col-md-12 my-auto">
             {{-- {{ddd($item)}} --}}
@@ -51,7 +77,9 @@
 
 
                 <div class="col-auto d-none d-lg-block">
-                    <img class="" alt="" src="{{asset('asset/item/images/'.$item->Id.'/Cart1.jpg')}}" id="image">
+                    <a href="#" id="pop">
+                    <img class="imagezoom" alt="" src="{{asset('asset/item/images/'.$item->Id.'/Cart1.jpg')}}" id="image">
+                    </a>
                         <div class="mt-2">
                             <img class="img-thumbnail" alt="" src="{{asset('asset/item/images/'.$item->Id.'/Cart1.jpg')}}" width="50" alt=" ">
                             <img class="img-thumbnail" alt="" src="{{asset('asset/item/images/'.$item->Id.'/Cart2.jpg')}}" width="50" alt=" ">
@@ -78,35 +106,117 @@
                             <td>{{$caracteristique->Value}}</td>
                         </tr>
                          @endforeach
-                         
+
                     @endif
                 </tbody>
               </table>
-              <button  id="plusinfo" class=" btn btn-primary">Plus de détails</button>
+               <button  id="plusinfo" class=" btn btn-success">Plus de détails</button>
+              <button  id="moinsinfo" class=" btn btn-warning" >Moins de détails</button>
+
+
         </div>
     </div>
+    <a href="#" id="pop">
+        <img id="imageresource" src="http://patyshibuya.com.br/wp-content/uploads/2014/04/04.jpg" style="width: 400px; height: 264px;">
+        Click to Enlarge
+    </a>
+
+    <!-- Creates the bootstrap modal where the image will appear -->
+    <div class="modal fade mt-4" id="imagemodal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true" >
+    <div class="modal-dialog">
+         {{--  <div class="modal-content">
+          <div class="modal-header">--}}
 
 
+          </div>
+          <div class="modal-body d-flex justify-content-center pt-4" >
+            <button  type="button" class="close" style="color: red" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+
+            <img src="{{asset('asset/item/images/'.$item->Id.'/Medium1.jpg')}}" id="imagepreview" style="width:1200px; height:auto;" >
+          </div>
+           <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+          </div>
+        </div>
+      </div>
+    </div>
+    <script>
+    $("#pop").on("click", function() {
+         $('#imagepreview').attr('src', $('#imagezoom').attr('src')); // here asign the image to the modal when the user click the enlarge link
+         $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+      });
+     </script>
+{{--
+    <div class="modal fade mt-4" id="imagemodal" tabindex="-1" data-dismiss="modal"s role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+        <div class="model-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+
+            <div class=" d-flex justify-content-center pt-4" data-dismiss="modal" >
+                <button type="button" class="close" data-dismiss="modal"><span aria-hidden="true">&times;</span><span class="sr-only">Close</span></button>
+
+
+              <img src="{{asset('asset/item/images/'.$item->Id.'/Medium1.jpg')}}" id="imagepreview" style="width:1200px; height:auto;" >
+            </div>
+             <div class="modal-footer">
+              <button type="button" class="btn btn-warning" data-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <script>
+          $("#pop").on("click", function() {
+         $('#imagepreview').attr('src', $('#imagezoom').attr('src')); // here asign the image to the modal when the user click the enlarge link
+         $('#imagemodal').modal('show'); // imagemodal is the id attribute assigned to the bootstrap modal, then i use the show function
+      });
+
+          </script> --}}
 
 
     <script>
+         $('#moinsinfo').hide();
+
         var thumbImage =document.querySelectorAll('.img-thumbnail');
                   thumbImage.forEach((element) => element.addEventListener('mouseenter', changeImage));
-                function changeImage(element){
-                        var data = $(this).find('img').data('zoom-image');
-                        var image = document.getElementById('image');
-                        image.setAttribute("src", this.src);
-                  }
+                 function changeImage(element){
+                         var data = $(this).find('img').data('zoom-image');
+                         var image = document.getElementById('image');
+                         image.setAttribute("src", this.src);
+                   }
 
                   $(function () {
-                    $('#plusinfo').click(function () {
-                        $('#datalist tr:hidden').slice(0, 60).show();
-                        if ($('#datalist tr').length == $('#datalist tr:visible').length) {
-                            $('#plusinfo').hide();
-                        }
+                     $('#plusinfo').click(function () {
+                         $('#datalist tr:hidden').slice(0, 60).show();
+                         if ($('#datalist tr').length == $('#datalist tr:visible').length) {
+                             $('#plusinfo').hide();
+                             $('#moinsinfo').show();
+                         }
+                     });
+                 });
+
+                 $(function () {
+                     $('#moinsinfo').click(function () {
+                         $('#datalist tr:visible ').slice(5, 150).hide();
+                         if ($('#datalist tr').length !== $('#datalist tr:visible').length) {
+                             $('#plusinfo').show();
+                             $('#moinsinfo').hide();
+
+
+
+
+                         }
+
+
+
+
+
+
+
                     });
-                });  
-                  
+                 });
+
+
+
     </script>
 
 @endsection
