8b7633d941f5015d04e5116019c23db2ef17c4d1
Mrassi DHIA <mrassi@RAIC.local>
2021-05-20 15:53:50 +0200

    slid

--- resources/views/include/navbarSlide.blade.php
+++ resources/views/include/navbarSlide.blade.php
@@ -1,3 +1,334 @@
+
+<div  class="row1 align-items-center">
+
+    <li class="nav-item dropdown" style="list-style-type: none;">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        Composants
+        </a>
+
+        <div class="dropdown-menu pt-0" aria-labelledby="navbarDropdown">
+
+
+
+            <div class="d-flex align-items-start flex-column flex-sm-row p-3">
+
+                <div>
+                    <div class="dropdown-header" >Processeurs</div>
+                    <a class="dropdown-item" href="#" >Développement web</a>
+                    <a class="dropdown-item" href="#" >Développement mobile</a>
+                    <a class="dropdown-item" href="#" >UX et Design</a>
+                    <a class="dropdown-item" href="#" >Infographie</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Memoires</div>
+                    <a class="dropdown-item" href="#" >HTML et CSS</a>
+                    <a class="dropdown-item" href="#" >BOOTSTRAP</a>
+                    <a class="dropdown-item" href="#" >PHP et LARAVEL</a>
+                    <a class="dropdown-item" href="#" >JAVASCRIPT et VUE.JS</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Ventilateur PC</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Alimentation Pour Boîtiers PC</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Cartes Graphiques</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+
+
+
+
+            </div>
+
+        </div>
+
+    </li>
+
+    <li class="nav-item dropdown" style="list-style-type: none;">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        PERIPHERIQUES
+        </a>
+
+        <div class="dropdown-menu pt-0" aria-labelledby="navbarDropdown">
+
+            <img src="./img/cover.jpg" class="img-fluid" alt="" >
+
+            <div class="d-flex align-items-start flex-column flex-sm-row p-3">
+
+                <div>
+                    <div class="dropdown-header">Services</div>
+                    <a class="dropdown-item" href="#" >Développement web</a>
+                    <a class="dropdown-item" href="#" >Développement mobile</a>
+                    <a class="dropdown-item" href="#" >UX et Design</a>
+                    <a class="dropdown-item" href="#" >Infographie</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Technologies</div>
+                    <a class="dropdown-item" href="#" >HTML et CSS</a>
+                    <a class="dropdown-item" href="#" >BOOTSTRAP</a>
+                    <a class="dropdown-item" href="#" >PHP et LARAVEL</a>
+                    <a class="dropdown-item" href="#" >JAVASCRIPT et VUE.JS</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Outils</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </li>
+    <li class="nav-item dropdown" style="list-style-type: none;">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        ORDINATEURS
+        </a>
+
+        <div class="dropdown-menu pt-0" aria-labelledby="navbarDropdown">
+
+            <img src="./img/cover.jpg" class="img-fluid" alt="" >
+
+            <div class="d-flex align-items-start flex-column flex-sm-row p-3">
+
+                <div>
+                    <div class="dropdown-header">Services</div>
+                    <a class="dropdown-item" href="#" >Développement web</a>
+                    <a class="dropdown-item" href="#" >Développement mobile</a>
+                    <a class="dropdown-item" href="#" >UX et Design</a>
+                    <a class="dropdown-item" href="#" >Infographie</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Technologies</div>
+                    <a class="dropdown-item" href="#" >HTML et CSS</a>
+                    <a class="dropdown-item" href="#" >BOOTSTRAP</a>
+                    <a class="dropdown-item" href="#" >PHP et LARAVEL</a>
+                    <a class="dropdown-item" href="#" >JAVASCRIPT et VUE.JS</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Outils</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </li>
+    <li class="nav-item dropdown" style="list-style-type: none;">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        MONITEURS
+        </a>
+
+        <div class="dropdown-menu pt-0" aria-labelledby="navbarDropdown">
+
+            <img src="./img/cover.jpg" class="img-fluid" alt="" >
+
+            <div class="d-flex align-items-start flex-column flex-sm-row p-3">
+
+                <div>
+                    <div class="dropdown-header">Services</div>
+                    <a class="dropdown-item" href="#" >Développement web</a>
+                    <a class="dropdown-item" href="#" >Développement mobile</a>
+                    <a class="dropdown-item" href="#" >UX et Design</a>
+                    <a class="dropdown-item" href="#" >Infographie</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Technologies</div>
+                    <a class="dropdown-item" href="#" >HTML et CSS</a>
+                    <a class="dropdown-item" href="#" >BOOTSTRAP</a>
+                    <a class="dropdown-item" href="#" >PHP et LARAVEL</a>
+                    <a class="dropdown-item" href="#" >JAVASCRIPT et VUE.JS</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Outils</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </li>
+    <li class="nav-item dropdown" style="list-style-type: none;">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        TELEPHONIE
+        </a>
+
+        <div class="dropdown-menu pt-0" aria-labelledby="navbarDropdown">
+
+            <img src="./img/cover.jpg" class="img-fluid" alt="" >
+
+            <div class="d-flex align-items-start flex-column flex-sm-row p-3">
+
+                <div>
+                    <div class="dropdown-header">Tablettes</div>
+                    <a class="dropdown-item" href="#" >Développement web</a>
+                    <a class="dropdown-item" href="#" >Développement mobile</a>
+                    <a class="dropdown-item" href="#" >UX et Design</a>
+                    <a class="dropdown-item" href="#" >Infographie</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Apple</div>
+                    <a class="dropdown-item" href="#" >HTML et CSS</a>
+                    <a class="dropdown-item" href="#" >BOOTSTRAP</a>
+                    <a class="dropdown-item" href="#" >PHP et LARAVEL</a>
+                    <a class="dropdown-item" href="#" >JAVASCRIPT et VUE.JS</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Accessoires</div>
+
+                </div>
+                <div>
+                    <div class="dropdown-header">Téléphones Mobiles</div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </li>
+    <li class="nav-item dropdown" style="list-style-type: none;">
+
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+        RESEAU
+        </a>
+
+        <div class="dropdown-menu pt-0" aria-labelledby="navbarDropdown">
+
+            <img src="./img/cover.jpg" class="img-fluid" alt="" >
+
+            <div class="d-flex align-items-start flex-column flex-sm-row p-3">
+
+                <div>
+                    <div class="dropdown-header">Services</div>
+                    <a class="dropdown-item" href="#" >Développement web</a>
+                    <a class="dropdown-item" href="#" >Développement mobile</a>
+                    <a class="dropdown-item" href="#" >UX et Design</a>
+                    <a class="dropdown-item" href="#" >Infographie</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Technologies</div>
+                    <a class="dropdown-item" href="#" >HTML et CSS</a>
+                    <a class="dropdown-item" href="#" >BOOTSTRAP</a>
+                    <a class="dropdown-item" href="#" >PHP et LARAVEL</a>
+                    <a class="dropdown-item" href="#" >JAVASCRIPT et VUE.JS</a>
+                </div>
+
+                <div>
+                    <div class="dropdown-header">Outils</div>
+                    <a class="dropdown-item" href="#" >Visual Studio Code</a>
+                    <a class="dropdown-item" href="#" >Laragon</a>
+                    <a class="dropdown-item" href="#" >Google Chrome</a>
+                    <a class="dropdown-item" href="#" >Windows 10</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </li>
+
+</div>
+
+
+
+{{--
+<nav id="menu_horizontale">
+  <ul>
+    <li class="deroulant"><a href="#">COMPOSANTS&ensp;</a>
+        <ul class="sous">
+            <li><a href="#">CSS display</a></li>
+            <li><a href="#">CSS position</a></li>
+            <li><a href="#">CSS float</a></li>
+          </ul>
+        </li>
+    <li class="deroulant"><a href="#">PERIPHERIQUES&ensp;</a>
+        <ul class="sous">
+             <li><a href="#">CSS display</a></li>
+             <li><a href="#">CSS position</a></li>
+             <li><a href="#">CSS float</a></li>
+             </ul>
+         </li>
+        <li class="deroulant"><a href="#">ORDINATEURS&ensp;</a>
+            <ul class="sous">
+                <li><a href="#">CSS display</a></li>
+                <li><a href="#">CSS position</a></li>
+                <li><a href="#">CSS float</a></li>
+              </ul>
+            </li>
+
+
+    <li class="deroulant"><a href="#">MONITEURS &ensp;</a>
+      <ul class="sous">
+        <li><a href="#">CSS display</a></li>
+        <li><a href="#">CSS position</a></li>
+        <li><a href="#">CSS float</a></li>
+      </ul>
+    </li>
+    <li class="deroulant"><a href="#">TELEPHONIE &ensp;</a>
+        <ul class="sous">
+          <li><a href="#">CSS display</a></li>
+          <li><a href="#">CSS position</a></li>
+          <li><a href="#">CSS float</a></li>
+        </ul>
+      </li>
+      <li class="deroulant"><a href="#">RESEAU &ensp;</a>
+        <ul class="sous">
+          <li><a href="#">CSS display</a></li>
+          <li><a href="#">CSS position</a></li>
+          <li><a href="#">CSS float</a></li>
+        </ul>
+      </li>
+    <li><a href="{{route('contact')}}">Contact</a></li>
+
+  </ul>
+</nav> --}}
+
+
+<!--
+
 @if (isset($Families))
 <nav class="sidebar">
     <div id="leftside-navigation" class="nano">
@@ -38,5 +369,8 @@
         //         e.stopPropagation()
         // });
 
+
     </script>
 @endsection
+
+-->
