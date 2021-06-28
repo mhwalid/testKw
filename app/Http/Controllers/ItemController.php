92cbecce296f51ae2f103d7226abdc43e9de62d5
walid hallouli <64472285+mhwalid@users.noreply.github.com>
2021-06-28 09:07:58 +0200

    Update ItemController.php

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -272,104 +272,104 @@ class ItemController extends Controller
         }
     }
 
-    public function filters(Request $rq){
-        $Id=$rq->FamilyId;
-        $marques = Db::connection('mysql')->table('main_carac')->select('marque')->distinct()->where('family', $Id)->get();
-        $memoire= Db::connection('mysql')->table('main_carac')->select('memoire')->distinct()->where('family', $Id)->get();
-        $taille_ecran= Db::connection('mysql')->table('main_carac')->select('taille_ecran')->distinct()->where('family', $Id)->get();
-        $ssd= Db::connection('mysql')->table('main_carac')->select('ssd')->distinct()->where('family', $Id)->get();
-        $os= Db::connection('mysql')->table('main_carac')->select('os')->distinct()->where('family', $Id)->get();
-        $chipset= Db::connection('mysql')->table('main_carac')->select('chipset')->distinct()->where('family', $Id)->get();
-        $fam_proc= Db::connection('mysql')->table('main_carac')->select('fam_proc')->distinct()->where('family', $Id)->get();
-        $sock_proc= Db::connection('mysql')->table('main_carac')->select('sock_proc')->distinct()->where('family', $Id)->get();
-        $gpu= Db::connection('mysql')->table('main_carac')->select('gpu')->distinct()->where('family', $Id)->get();
-        $puissance= Db::connection('mysql')->table('main_carac')->select('puissance')->distinct()->where('family', $Id)->get();
-        $frequ_mem= Db::connection('mysql')->table('main_carac')->select('frequ_memoire')->distinct()->where('family', $Id)->get();
-        $nb_barrette= Db::connection('mysql')->table('main_carac')->select('nb_barrette')->distinct()->where('family', $Id)->get();
-        $Families = Family::all()->groupBy('MainIntervener');
-        $fmarques = $rq->marques;
-        $fmemoire = $rq->memoire;
-        $ftaille_ecran = $rq->taille_ecran;  
-        $fssd = $rq->ssd;  
-        $fos = $rq->os;  
-        $ffam_proc = $rq->fam_proc;  
-        $fsock_proc = $rq->sock_proc;  
-        $fgpu = $rq->gpu;  
-        $fchipset = $rq->chipset;  
-        $fpuissance = $rq->puissance;  
-        $ffrequ_mem = $rq->frequ_mem;  
-        $fnb_barrette = $rq->nb_barrette;
-        $url= (explode('/', $rq->url)); 
-        $MainCarac = DB::Table('main_carac')->where('family', $Id);
-        $checked="";
-        // return dd($rq->all());
-        $i=0;
-        foreach($rq->all() as $index){
-            $i++;
-            if($i >=5){
-                if(is_array($index) || is_object($index)){
-                    foreach($index as $value){
-                        $checked.= $value.";";
-                    }
-                   }else{
-                    $checked.= $index.";";
-                   }
-              }
-       }
-        // return $test = DB::Table('main_carac')->select('id_item')->where('marque','ASUS')->orwhere('marque','msi')->get();
-        if(isset($rq->marques)){
-                $MainCarac->where(function ($query) use ($fmarques){
-                    foreach($fmarques as $index =>$mar){
-                         if($index ==0){
-                            $query->where('marque',$mar);
-                           }else{
-                            $query->orwhere('marque',$mar);
-                           }
-                    }
-                    return  $query;
-             });
-        }
-        if(isset($rq->memoire)){
-            $MainCarac->where(function ($query) use ($fmemoire){
-                foreach($fmemoire as $index =>$value){
-                     if($index ==0){
-                        $query->where('memoire',$value);
-                       }else{
-                        $fmemoire->orwhere('memoire',$value);
-                       }
-                }
-                return  $query;
-            });
-        }
+//     public function filters(Request $rq){
+//         $Id=$rq->FamilyId;
+//         $marques = Db::connection('mysql')->table('main_carac')->select('marque')->distinct()->where('family', $Id)->get();
+//         $memoire= Db::connection('mysql')->table('main_carac')->select('memoire')->distinct()->where('family', $Id)->get();
+//         $taille_ecran= Db::connection('mysql')->table('main_carac')->select('taille_ecran')->distinct()->where('family', $Id)->get();
+//         $ssd= Db::connection('mysql')->table('main_carac')->select('ssd')->distinct()->where('family', $Id)->get();
+//         $os= Db::connection('mysql')->table('main_carac')->select('os')->distinct()->where('family', $Id)->get();
+//         $chipset= Db::connection('mysql')->table('main_carac')->select('chipset')->distinct()->where('family', $Id)->get();
+//         $fam_proc= Db::connection('mysql')->table('main_carac')->select('fam_proc')->distinct()->where('family', $Id)->get();
+//         $sock_proc= Db::connection('mysql')->table('main_carac')->select('sock_proc')->distinct()->where('family', $Id)->get();
+//         $gpu= Db::connection('mysql')->table('main_carac')->select('gpu')->distinct()->where('family', $Id)->get();
+//         $puissance= Db::connection('mysql')->table('main_carac')->select('puissance')->distinct()->where('family', $Id)->get();
+//         $frequ_mem= Db::connection('mysql')->table('main_carac')->select('frequ_memoire')->distinct()->where('family', $Id)->get();
+//         $nb_barrette= Db::connection('mysql')->table('main_carac')->select('nb_barrette')->distinct()->where('family', $Id)->get();
+//         $Families = Family::all()->groupBy('MainIntervener');
+//         $fmarques = $rq->marques;
+//         $fmemoire = $rq->memoire;
+//         $ftaille_ecran = $rq->taille_ecran;  
+//         $fssd = $rq->ssd;  
+//         $fos = $rq->os;  
+//         $ffam_proc = $rq->fam_proc;  
+//         $fsock_proc = $rq->sock_proc;  
+//         $fgpu = $rq->gpu;  
+//         $fchipset = $rq->chipset;  
+//         $fpuissance = $rq->puissance;  
+//         $ffrequ_mem = $rq->frequ_mem;  
+//         $fnb_barrette = $rq->nb_barrette;
+//         $url= (explode('/', $rq->url)); 
+//         $MainCarac = DB::Table('main_carac')->where('family', $Id);
+//         $checked="";
+//         // return dd($rq->all());
+//         $i=0;
+//         foreach($rq->all() as $index){
+//             $i++;
+//             if($i >=5){
+//                 if(is_array($index) || is_object($index)){
+//                     foreach($index as $value){
+//                         $checked.= $value.";";
+//                     }
+//                    }else{
+//                     $checked.= $index.";";
+//                    }
+//               }
+//        }
+//         // return $test = DB::Table('main_carac')->select('id_item')->where('marque','ASUS')->orwhere('marque','msi')->get();
+//         if(isset($rq->marques)){
+//                 $MainCarac->where(function ($query) use ($fmarques){
+//                     foreach($fmarques as $index =>$mar){
+//                          if($index ==0){
+//                             $query->where('marque',$mar);
+//                            }else{
+//                             $query->orwhere('marque',$mar);
+//                            }
+//                     }
+//                     return  $query;
+//              });
+//         }
+//         if(isset($rq->memoire)){
+//             $MainCarac->where(function ($query) use ($fmemoire){
+//                 foreach($fmemoire as $index =>$value){
+//                      if($index ==0){
+//                         $query->where('memoire',$value);
+//                        }else{
+//                         $fmemoire->orwhere('memoire',$value);
+//                        }
+//                 }
+//                 return  $query;
+//             });
+//         }
 
-        if(isset($rq->memoire)){
-            $MainCarac->where(function ($query) use ($fmemoire){
-                foreach($fmemoire as $index =>$value){
-                     if($index ==0){
-                        $query->where('memoire',$value);
-                       }else{
-                        $fmemoire->orwhere('memoire',$value);
-                       }
-                }
-                return  $query;
-            });
-        }
-        if(isset($rq->memoire)){
-            $MainCarac->where(function ($query) use ($fmemoire){
-                foreach($fmemoire as $index =>$value){
-                     if($index ==0){
-                        $query->where('memoire',$value);
-                       }else{
-                        $fmemoire->orwhere('memoire',$value);
-                       }
-                }
-                return  $query;
-            });
-        }
-        $MainCarac= $MainCarac->pluck('id_item');
-        $items= Item::itemA()->whereIn('Id', $MainCarac)->paginate(20);
-        return view('product.home', compact('items','checked', 'Families','marques','memoire','taille_ecran','ssd','os','chipset','fam_proc','sock_proc','gpu','puissance','frequ_mem','nb_barrette'));
-    }
+//         if(isset($rq->memoire)){
+//             $MainCarac->where(function ($query) use ($fmemoire){
+//                 foreach($fmemoire as $index =>$value){
+//                      if($index ==0){
+//                         $query->where('memoire',$value);
+//                        }else{
+//                         $fmemoire->orwhere('memoire',$value);
+//                        }
+//                 }
+//                 return  $query;
+//             });
+//         }
+//         if(isset($rq->memoire)){
+//             $MainCarac->where(function ($query) use ($fmemoire){
+//                 foreach($fmemoire as $index =>$value){
+//                      if($index ==0){
+//                         $query->where('memoire',$value);
+//                        }else{
+//                         $fmemoire->orwhere('memoire',$value);
+//                        }
+//                 }
+//                 return  $query;
+//             });
+//         }
+//         $MainCarac= $MainCarac->pluck('id_item');
+//         $items= Item::itemA()->whereIn('Id', $MainCarac)->paginate(20);
+//         return view('product.home', compact('items','checked', 'Families','marques','memoire','taille_ecran','ssd','os','chipset','fam_proc','sock_proc','gpu','puissance','frequ_mem','nb_barrette'));
+//     }
     public function feature($id){
         $item = Item::find($id);
         $arrivage = Db::connection('sqlsrv')->table('PurchaseDocumentLine')->select('PurchaseDocumentLine.Quantity', 'PurchaseDocumentLine.DeliveryDate', 'item.Id')->join('item', 'item.Id', '=', 'PurchaseDocumentLine.ItemId')->where('item.Id', $id)->whereRaw('DeliveryDate > SYSDATETIME()')->first();
